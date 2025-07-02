b32c5f28fbb8c2589e7cb6c3e1483244077cc0c8
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:33:44 +0500

    Update output.tf

--- three_tier_app/rds-resource/output.tf
+++ three_tier_app/rds-resource/output.tf
@@ -1,1 +1,11 @@
 
+output "db_host" {
+  description = "RDS endpoint address"
+  value       = aws_db_instance.this.address
+}
+
+output "db_port" {
+  description = "RDS port"
+  value       = aws_db_instance.this.port
+}
+
