7c9996e5f9ddc4206d2fca6cda028d0e9188105b
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:37:07 +0500

    Update variable.tf

--- three_tier_app/sg-resource/variable.tf
+++ three_tier_app/sg-resource/variable.tf
@@ -1,1 +1,10 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  type        = string
+}
 
