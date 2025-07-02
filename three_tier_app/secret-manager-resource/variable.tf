85bacdf3e087cde57a824a9ceac21e4cb159c2a8
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:36:01 +0500

    Update variable.tf

--- three_tier_app/secret-manager-resource/variable.tf
+++ three_tier_app/secret-manager-resource/variable.tf
@@ -1,1 +1,33 @@
+variable "secret_name" {
+  description = "Name of the secret"
+  type        = string
+  default     = "guestbook-secret"
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_host" {
+  description = "RDS host endpoint"
+  type        = string
+}
+
+variable "db_port" {
+  description = "RDS port"
+  type        = number
+  default     = 3306
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
 
