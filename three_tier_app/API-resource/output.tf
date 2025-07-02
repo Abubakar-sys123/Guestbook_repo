b076d47e5a899aa206bd8f49b03c494066af93bf
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:28:35 +0500

    Update output.tf

--- three_tier_app/API-resource/output.tf
+++ three_tier_app/API-resource/output.tf
@@ -1,1 +1,9 @@
 
+output "api_id" {
+  value = aws_api_gateway_rest_api.this.id
+}
+
+output "api_endpoint" {
+  value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.region}.amazonaws.com/dev"
+}
+
