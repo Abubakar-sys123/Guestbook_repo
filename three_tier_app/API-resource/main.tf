24aefe6de299aafc1b92ade36975f84f8bd6b719
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:28:23 +0500

    Update main.tf

--- three_tier_app/API-resource/main.tf
+++ three_tier_app/API-resource/main.tf
@@ -1,1 +1,61 @@
+resource "aws_api_gateway_rest_api" "this" {
+  name        = var.api_name
+  description = "Guestbook REST API"
+}
 
+resource "aws_api_gateway_resource" "guest" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "guest"
+}
+
+resource "aws_api_gateway_method" "post_guest" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.guest.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "get_guest" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.guest.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.guest.id
+  http_method             = aws_api_gateway_method.post_guest.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_integration" "get_lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.guest.id
+  http_method             = aws_api_gateway_method.get_guest.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "this" {
+  depends_on  = [aws_api_gateway_integration.get_lambda, aws_api_gateway_integration.post_lambda]
+  rest_api_id = aws_api_gateway_rest_api.this.id
+}
+resource "aws_api_gateway_stage" "dev" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "dev"
+}
+
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway-${var.api_name}"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/*"
+}
