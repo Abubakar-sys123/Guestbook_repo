0a0cf00a81731d52fc48d8ec07bb90c5549a4196
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:36:43 +0500

    Update output.tf

--- three_tier_app/sg-resource/output.tf
+++ three_tier_app/sg-resource/output.tf
@@ -1,1 +1,20 @@
+output "lambda_sg_id" {
+  description = "Security Group ID for Lambda"
+  value       = aws_security_group.lambda_sg.id
+}
+
+output "rds_sg_id" {
+  description = "Security Group ID for RDS"
+  value       = aws_security_group.rds_sg.id
+}
+
+output "rds_endpoint_sg_id" {
+  description = "Security Group ID for RDS VPC Endpoint"
+  value       = aws_security_group.rds_endpoint_sg.id
+}
+
+output "secrets_endpoint_sg_id" {
+  description = "Security Group ID for Secrets Manager VPC Endpoint"
+  value       = aws_security_group.secrets_endpoint_sg.id
+}
 
