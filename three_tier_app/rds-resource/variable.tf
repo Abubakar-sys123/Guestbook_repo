d827f985c66a1adead1be7e4b4ffea9bd4ca1694
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:33:59 +0500

    Update variable.tf

--- three_tier_app/rds-resource/variable.tf
+++ three_tier_app/rds-resource/variable.tf
@@ -1,1 +1,19 @@
+variable "private_subnet_ids" {
+  description = "Private subnet IDs for the RDS instance"
+  type        = list(string)
+}
+
+variable "rds_sg_id" {
+  description = "Security group ID for RDS instance"
+  type        = string
+}
+
+variable "secret_name" {
+  description = "Name of the existing secret in Secrets Manager"
+  type        = string
+}
+variable "vpc_id" {
+  description = "VPC ID for the RDS instance"
+  type        = string
+}
 
