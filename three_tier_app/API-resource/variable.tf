f63ece26f9a5c277750475848b49fad86add4f5f
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:28:50 +0500

    Update variable.tf

--- three_tier_app/API-resource/variable.tf
+++ three_tier_app/API-resource/variable.tf
@@ -1,1 +1,21 @@
 
+variable "api_name" {
+  description = "Name of the API"
+  type        = string
+}
+
+variable "lambda_invoke_arn" {
+  description = "Invoke ARN of the Lambda function"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "Lambda function name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
