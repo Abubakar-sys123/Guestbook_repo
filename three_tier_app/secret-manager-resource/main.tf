bcd96da00bed920dec7cfb8ca617913a1217010f
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:35:22 +0500

    Update main.tf

--- three_tier_app/secret-manager-resource/main.tf
+++ three_tier_app/secret-manager-resource/main.tf
@@ -1,1 +1,16 @@
+resource "aws_secretsmanager_secret" "this" {
+  name        = var.secret_name
+  description = "RDS credentials for the guestbook app"
+}
+
+resource "aws_secretsmanager_secret_version" "this" {
+  secret_id     = aws_secretsmanager_secret.this.id
+  secret_string = jsonencode({
+    username = var.db_username
+    password = var.db_password
+    host     = var.db_host
+    port     = var.db_port
+    dbname   = var.db_name
+  })
+}
 
