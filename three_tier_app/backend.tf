eba44c351cdb4fec1a968a13cff1a37134e00f6f
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:38:48 +0500

    Update backend.tf

--- three_tier_app/backend.tf
+++ three_tier_app/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "backend-abubakar19"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "Terraform-lock"
+    encrypt        = true
+  }
+}
 
