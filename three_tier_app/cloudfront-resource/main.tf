2a3a70cb889c9eeca79f516dcf9282e89f637c83
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:29:11 +0500

    Update main.tf

--- three_tier_app/cloudfront-resource/main.tf
+++ three_tier_app/cloudfront-resource/main.tf
@@ -1,1 +1,50 @@
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "guestbook-oac"
+  description                       = "OAC for S3 guestbook frontend"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
 
+resource "aws_cloudfront_distribution" "this" {
+  enabled             = true
+  default_root_object = "index1.html"
+
+  origin {
+    domain_name = "${var.bucket_name}.s3.amazonaws.com"
+    origin_id   = var.bucket_name
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = var.bucket_name
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "guestbook-cloudfront"
+  }
+}
