20dd3845105aaafbd0dfd0ea9a9dc24e2fbd382c
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-02 19:30:41 +0500

    Update main.tf

--- three_tier_app/lambda-resource/main.tf
+++ three_tier_app/lambda-resource/main.tf
@@ -1,1 +1,75 @@
 
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "guestbook-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_inline_policy" {
+  name = "lambda-inline-policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ],
+        Resource = var.secret_arn
+      }
+    ]
+  })
+}
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = var.api_gateway_invoke_arn
+}
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = var.handler
+  runtime       = var.runtime
+  filename      = var.lambda_zip_path
+  timeout       = 15
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [var.lambda_sg_id]
+  }
+
+  layers = [var.layer_arn]
+
+  environment {
+    variables = {
+      SECRET_NAME = var.secret_name
+      REGION_NAME = var.region
+    }
+  }
+}
+
